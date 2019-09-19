8023fd4e6026d603aa0d93601b2a887ae83e6b25
unknown <15297879246@163.com>
2019-09-19 17:14:40 +0800

    首页面

---
+++ README.md
@@ -0,0 +1,2 @@
+#前端框架
+semantic ui官方文档:https://semantic-ui.com/
---
+++ src/main/java/blogdemo/demo/controller/BaseController.java
@@ -0,0 +1,18 @@
+package blogdemo.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author admin
+ * @date 2019/9/19 10:58
+ */
+
+@Controller
+public class BaseController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
